931f3676c8e3267c83be1e6b24271b7bcc7c80e5
lihtgoltA <a.lihtgolt@qorporates.com>
2022-09-11 16:44:48 +0300

    firstLesson

---
+++ lesson-01-js/script.js
@@ -0,0 +1,35 @@
+// alert('Hello World!');
+// console.log("Hello World!");
+
+// let peremenayaDliaSummy;
+
+// a = 10;
+
+// a = 15;
+// b = 25;
+
+// console.log(a + b);
+
+// a = 20;
+
+// console.log(a + b);
+
+// let stroka = "String";
+// console.log(stroka);
+// let chislo = 42;
+// console.log(chislo);
+// let bool = true; // false
+// console.log(bool);
+
+// stroka = 500;
+// console.log(stroka);
+
+// let x = 5;
+// let y = x + 3;
+// let z = y * 2 + (x - 3);
+// let k = (z / 2) * 2;
+
+// console.log(x);
+// console.log(y);
+// console.log(z);
+// console.log(k);
